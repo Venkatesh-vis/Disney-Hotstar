5528d9ef193f2d002252168aa2489ff98cab9906
Venkatesh-vis <venkateshvishwanadula257@gmail.com>
2024-07-04 12:50:02 +0530

    ..

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAivW9qQ7s9QMZuII__obuH8fIO_uUKzzE",
+    authDomain: "disney-clone-a348e.firebaseapp.com",
+    projectId: "disney-clone-a348e",
+    storageBucket: "disney-clone-a348e.appspot.com",
+    messagingSenderId: "305085685671",
+    appId: "1:305085685671:web:a342787a96af22d8f2a082",
+    measurementId: "G-EGLQME5M9W"
+  };
+
+  
